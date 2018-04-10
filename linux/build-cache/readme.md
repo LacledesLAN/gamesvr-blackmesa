578236c96ef70ce40e3b70648f9142404cd18c13
James Dudley <jedudley@outlook.com>
2018-04-09 22:06:13 -0500

    SteamCMD caching now properly implemented

--- linux/build-cache/readme.md
+++
@@ -1,1 +0,0 @@
-This folder can be used for caching Black Mesa prior to docker build - use SteamCMD to install the game data into this directory.
--- linux/ll-tests/gamesvr-blackmesa.sh
+++ linux/ll-tests/gamesvr-blackmesa.sh
@@ -145,12 +145,17 @@ fi;
 
 #####################################################################################################
 ### TESTS ###########################################################################################
+# Stock Black Mesa Source server tests
 should_have 'Using default binary: ./srcds_linux' 'Sever started executing';
 should_lack 'Server restart in 10 seconds' 'Server is not boot-looping';
+should_lack 'Please install (lib32tinfo5 / ncurses-libs.i686 / equivalent) to enable readline' 'readline dependencies found';
 should_lack 'Running the dedicated server as root' 'Server is not running under root';
 should_have 'server_srv.so loaded for "Black Mesa"' 'srcds_run loaded Black Mesa';
+should_lack 'map load failed:' 'Server was able to load map';
 should_have 'Server is hibernating' 'srcds_run succesfully hibernated';
-should_echo "sv_cheats" '"sv_cheats" = "0"';
+
+# Verify server responds to commands
+should_echo "say STARTING COMMAND TESTS" 'Console: STARTING COMMAND TESTS';
 #####################################################################################################
 #####################################################################################################
 
